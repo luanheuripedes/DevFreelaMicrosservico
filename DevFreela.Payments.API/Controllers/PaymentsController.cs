e66f678332fa24a85cc169d3be25ae27b81e5c80
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-08-15 15:44:42 -0300

    Criado o microserviço de pagamento

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    public class PaymentsController:ControllerBase
+    {
+        private readonly PaymentService _paymentService;
+
+        public PaymentsController(PaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfoInputModel)
+        {
+            var result = await _paymentService.Process(paymentInfoInputModel);
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+    }
+}
